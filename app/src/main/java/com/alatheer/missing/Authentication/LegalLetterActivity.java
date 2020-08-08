5d289a711addb15424b579048774d70ae60a29c1
mohamedhamada2 <mohamedhamada2024@gmail.com>
2020-08-08 16:25:23 +0200

    lanuages

--- app/src/main/java/com/alatheer/missing/Authentication/LegalLetterActivity.java
+++ app/src/main/java/com/alatheer/missing/Authentication/LegalLetterActivity.java
@@ -12,5 +12,6 @@ public class LegalLetterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_legal_letter);
+
     }
 }
