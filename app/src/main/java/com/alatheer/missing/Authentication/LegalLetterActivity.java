2d0ab8818c8c6abc98a706a4d2c66011e2de3717
mohamedhamada2 <mohamedhamada2024@gmail.com>
2020-08-08 16:24:55 +0200

    lanuages

---
+++ app/src/main/java/com/alatheer/missing/Authentication/LegalLetterActivity.java
@@ -0,0 +1,16 @@
+package com.alatheer.missing.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alatheer.missing.R;
+
+public class LegalLetterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_legal_letter);
+    }
+}
